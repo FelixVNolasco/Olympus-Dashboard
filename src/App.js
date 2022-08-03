45f3e4db54fc966ec8ed05cc9807f0f1bb65a209
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-08-02 20:55:19 -0500

    added auth validation router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "./components/Auth/Login";
 import { Signup } from "./components/Auth/Signup";
 import { Home } from "./pages/Home";
@@ -6,18 +6,30 @@ import { Products } from "./pages/products/Products";
 import { NewProduct } from "./pages/products/NewProduct";
 import { Users } from "./pages/users/Users";
 import { Profile } from "./pages/Profile/Profile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { isAuthenticated } = useSelector((state) => state.user);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        {isAuthenticated ? (
+          <Route path="/" element={<Home />}></Route>
+        ) : (
+          <Route
+            path="/"
+            element={<Navigate to={"/auth/login"} replace />}
+          ></Route>
+        )}
+
         <Route path="/users" element={<Users />}></Route>
         <Route path="/product/new" element={<NewProduct />}></Route>
         <Route path="/products" element={<Products />}></Route>
         <Route path="/auth/login" element={<Login />}></Route>
         <Route path="/auth/signup" element={<Signup />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
