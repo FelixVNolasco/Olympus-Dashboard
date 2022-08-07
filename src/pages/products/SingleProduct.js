1ed0d6c554fbba9d591a8dd148719ce80412c63d
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-08-07 22:47:42 +0000

    added useEffect hook in the single user and prod

--- src/pages/products/SingleProduct.js
+++ src/pages/products/SingleProduct.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  doneFetchingData,
+  failFetchingData,
+  startFetchingData,
+} from "../../redux/userReducer";
 
 export const SingleProduct = () => {
+
+  const [product, setProduct] = useState();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const productId = location.pathname.split("/")[2];
+  const { isFetching } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const getProduct = () => {
+      dispatch(startFetchingData());
+      axios
+        .get(`https://olympus-backend.vercel.app/api/products/find/${productId}`)
+        .then((resp) => {
+          setProduct(resp.data);
+          dispatch(doneFetchingData());
+        })
+        .catch((error) => {
+          dispatch(failFetchingData());
+          console.log(error);
+        });
+    };
+    getProduct();
+  }, [dispatch, productId]);
+
+
   return (
-    <div>SingleProduct</div>
+
+    <>
+      {
+        isFetching ?
+          <span>Cargando...</span>
+          :
+          <div>
+            <span>{product?.title}</span>
+            <span>{product?.desc}</span>
+            <span>{product?.createdAt}</span>
+            <span>{product?.updatedAt}</span>
+            <span>{product?.price}</span>
+            <span>{product?.instock}</span>
+          </div>
+      }
+    </>
   )
+
 }
--- src/pages/users/SingleUser.js
+++ src/pages/users/SingleUser.js
@@ -1,5 +1,63 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import {
+  doneFetchingData,
+  failFetchingData,
+  startFetchingData,
+} from "../../redux/userReducer";
 
 export const SingleUser = () => {
-  return <></>;
+
+  const [user, setUser] = useState();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const userId = location.pathname.split("/")[2];
+  const { isFetching } = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const { accessToken } = currentUser;
+
+  useEffect(() => {
+
+    const config = {
+      headers: {
+        token: `Bearer ${accessToken}`,
+      },
+    };
+    const getUser = () => {
+      dispatch(startFetchingData());
+      axios
+        .get(`https://olympus-backend.vercel.app/api/users/find/${userId}`, config)
+        .then((resp) => {
+          setUser(resp.data);
+          dispatch(doneFetchingData());
+        })
+        .catch((error) => {
+          dispatch(failFetchingData());
+          console.log(error);
+        });
+    };
+    getUser();
+  }, [accessToken, dispatch, userId]);
+
+  return <>
+    {
+
+      isFetching ?
+
+        <span>Cargando...</span>
+        :
+
+        <div>
+          <span>{user?.createdAt}</span>
+          <span>{user?.email}</span>
+          <span>{user?.isAdmin}</span>
+          <span>{user?.updatedAt}</span>
+          <span>{user?.urlImage}</span>
+          <span>{user?.username}</span>
+        </div>
+
+
+    }</>;
 };
